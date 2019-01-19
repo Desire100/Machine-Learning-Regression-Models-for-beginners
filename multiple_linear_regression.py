050a784c4efa62f1e12c9cc34b6b180b0a3a01d6
Desire Iradukunda <35916017+Desire100@users.noreply.github.com>
2019-01-19 10:16:16 +0800

    Update multiple_linear_regression.py

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -6,19 +6,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('path and format of your dataset')
-X = dataset.iloc[:, :].values
-y = dataset.iloc[:, ].values
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
 
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder = LabelEncoder()
-X[:, 3] = labelencoder.fit_transform(X[:, ])
-onehotencoder = OneHotEncoder(categorical_features = [])
+X[:, 3] = labelencoder.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
 # Avoiding the Dummy Variable Trap
-X = X[:, :]
+X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
